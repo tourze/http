b88f45513bce0f6dfab40420bba9ec16837b2c0b
ywisax <25803471@qq.com>
2015-09-05 03:01:02 +0800

    增加两个组件的http别名

---
+++ src/Component/Log.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tourze\Http\Component;
+
+use tourze\Base\Component\Log as BaseLog;
+
+/**
+ * 日志组件
+ *
+ * @package tourze\Http\Component
+ */
+class Log extends BaseLog
+{
+}
---
+++ src/Component/Session.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tourze\Http\Component;
+
+use tourze\Base\Component\Session as BaseSession;
+
+/**
+ * 会话组件
+ *
+ * @package tourze\Http\Component
+ */
+class Session extends BaseSession
+{
+}
